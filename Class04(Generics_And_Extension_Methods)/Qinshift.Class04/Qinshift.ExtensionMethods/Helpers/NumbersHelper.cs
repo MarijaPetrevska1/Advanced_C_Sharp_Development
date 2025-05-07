c08ae5fc5bb737298355536c9e959978d5bf1325
Marija Petrevska <marija_petrevska25@yahoo.com>
2025-05-07 18:01:11 +0200

    Class04

---
+++ Class04(Generics_And_Extension_Methods)/Qinshift.Class04/Qinshift.ExtensionMethods/Helpers/NumbersHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace Qinshift.ExtensionMethods.Helpers
+{
+    public static class NumbersHelper
+    {
+        public static bool IsEven(this int value)
+        {
+            if (value == 0)
+            {
+                throw new ArgumentException("The number is 0");
+                return value % 2 == 0;
+            }
+        }
+    }
+
+}
