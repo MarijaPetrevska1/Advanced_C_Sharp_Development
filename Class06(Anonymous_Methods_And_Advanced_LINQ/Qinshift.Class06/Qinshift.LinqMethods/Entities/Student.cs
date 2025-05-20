b8e84da73c5dcc49e5f35d4c15abf31769c0aa9a
Marija Petrevska <marija_petrevska25@yahoo.com>
2025-05-20 14:47:44 +0200

    Class 06

---
+++ Class06(Anonymous_Methods_And_Advanced_LINQ/Qinshift.Class06/Qinshift.LinqMethods/Entities/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qinshift.LinqMethods.Entities
+{
+    public class Student:BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public bool IsPartTime { get; set; }
+        public List<Subject> Subjects { get; set; }
+        public Student()
+        {
+
+        }
+
+        public Student(int id, string first, string last, int age, bool partTime)
+        {
+            Id = id;
+            FirstName = first;
+            LastName = last;
+            Age = age;
+            IsPartTime = partTime;
+        }
+        public override string Info()
+        {
+            string result = $"{Id}) {FirstName} {LastName} - {Age} years old.";
+            result += IsPartTime ? "A part time student!" : "";
+            return result;
+        }
+    }
+}
