4a5b1b2251456d1fb5cb0d2085e7487fc063817c
Marija Petrevska <marija_petrevska25@yahoo.com>
2025-05-20 15:58:51 +0200

    Class07

---
+++ Class07(Events_And_Delegates)/Qinshift.Class07/Qinshift.Events/Entities/Person.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Channels;
+
+namespace Qinshift.Events.Entities
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public bool HasWokeUp { get; set; } = false;
+        public void WakeUp()
+        {
+            Console.WriteLine($"{Name} waaaaaaake uup!!!");
+            HasWokeUp = true;
+        }
+    }
+}
+
