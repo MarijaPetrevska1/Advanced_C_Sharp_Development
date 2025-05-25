ea5575b06aba36943134e5c538a92b51002606c5
Marija Petrevska <marija_petrevska25@yahoo.com>
2025-05-25 23:41:08 +0200

    Homework

---
+++ Class09(Reading_And_Writing_On_File_System)/Homework7/Task1/Program.cs
@@ -0,0 +1,41 @@
+﻿
+// Homework 7 - Task 1
+// Reading and Writing on File System
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("\n Task 1 - Homework 7\n");
+Console.ResetColor();
+
+string folderPath = @"..\..\..\Files";
+string filePath = Path.Combine(folderPath, "names.txt");
+
+// Create a folder named "Files"
+if (!Directory.Exists(folderPath))
+{
+    Directory.CreateDirectory(folderPath);
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Folder 'Files' was created.");
+    Console.ResetColor();
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("Folder 'Files' already exists.");
+    Console.ResetColor();
+}
+
+// Create a file name "names.txt"
+if (!File.Exists(filePath))
+{
+    File.Create(filePath).Close();
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("File 'names.txt' was created.");
+    Console.ResetColor();
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("File 'names.txt' already exists.");
+    Console.ResetColor();
+}
+
+Console.ReadLine();
